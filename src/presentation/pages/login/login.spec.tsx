5f137f4931d523f832de73ce38d3b130c823d0ec
Thalles Ian <thallesyam@users.=.github.com>
2022-05-10 21:03:16 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -59,4 +59,14 @@ describe('Login Component', () => {
       email: 'any_email'
     })
   })
+
+  test('should call Validation with correct password', () => {
+    const { sut: { getByTestId }, validationSpy } = makeSut()
+
+    const passwordInput = getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
