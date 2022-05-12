e0b9b7e23df683457a4149ecec9c0f0fdeda6efb
Thalles Ian <thallesyam@users.=.github.com>
2022-05-12 10:28:23 -0300

    feat: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -14,11 +14,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     email: '',
     password: '',
     mainError: '',
-    emailError: 'Campo obrigatório',
+    emailError: '',
     passwordError: 'Campo obrigatório'
   })
 
   useEffect(() => {
+    setState({
+      ...state,
+      emailError: validation.validate('email', state.email)
+    })
+
     validation.validate('email', state.email)
   }, [state.email])
 
