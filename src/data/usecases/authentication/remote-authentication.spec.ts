9642b659869f53ff87bad0f2baba30b114834024
Thalles Ian <thallesyam@gmail.com>
2024-07-23 21:14:46 -0300

    refactor: change RemoteAuthentication to use faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
+import faker from '@ngneat/falso'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url = 'any_url'): SutTypes => {
+const makeSut = (url = faker.randUrl()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy() 
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
