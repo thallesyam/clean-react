96cd197a51665c324f89b5c3b7928f60fc463edb
Thalles Ian <thallesyam@gmail.com>
2024-08-02 19:56:54 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,6 +56,16 @@ describe("<Login />", () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
+  test("should show valid email state if Validation succeeds", () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: randEmail() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
+
   test("should show valid password state if Validation succeeds", () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
